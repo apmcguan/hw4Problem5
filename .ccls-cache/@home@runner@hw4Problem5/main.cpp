a325fd410b57f8eeee45cb1cd7c06e95a7903d8b
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-19 06:12:38 +0000

    trial and error

--- .ccls-cache/@home@runner@hw4Problem5/main.cpp
+++ .ccls-cache/@home@runner@hw4Problem5/main.cpp
@@ -6,16 +6,24 @@ using namespace std;
 int main() {
   int N;
   cin >> N;
-  double V0, a, h1, h2, x1, g, r, t, y;
-  g = 9.8;
+  
+  double V0, a, h1, h2, x1, r, t, y;
+  const double g = 9.81;
   r = acos(-1) /180;
-  t = x1 / V0/ cos (a*r);
-  y - V0 * t * sin(a * r)- (0.5 *g * (t * t));
+ 
 
   
   for (int i = 0; i < N; i++)
     {
       cin >> V0 >> a >> h1 >> h2;
+    double t = x1 / V0/ cos (a*r);
+    double y = V0 * t * sin(a * r)- (0.5 *g * (t * t));
+
+      if (( y >= h1+1) && y<= h2-1)
+        cout << "Safe" << endl;
+
+      else 
+        cout <<"Not safe"<<endl;
       
       
     }
