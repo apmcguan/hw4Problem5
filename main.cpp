182b556130316eef6f315082281cd574e4921cdb
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-19 06:03:23 +0000

    t and y

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 int main() {
   int N;
   cin >> N;
-  double V0, a, h1, h2, x1, g, r ;
+  double V0, a, h1, h2, x1, g, r, t, y;
   g = 9.8;
   r = acos(-1) /180;
+  t = x1 / V0/ cos (a*r);
+  y - V0 * t * sin(a * r)- (0.5 *g * (t * t));
 
   
   for (int i = 0; i < N; i++)
     {
       cin >> V0 >> a >> h1 >> h2;
+      
       
     }
   
